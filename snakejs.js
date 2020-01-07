55e579dfa6c2007e6a97a5915f4754e6194c8b7a
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-01-07 01:06:14 +0100

    in progress

--- snakejs.js
+++ snakejs.js
@@ -23,13 +23,31 @@ var apple = {
     x : Math.floor(Math.random()*30)*20,
     y : Math.floor(Math.random()*30)*20,
 }
-console.log(apple);
+//console.log(apple);
 
-snakeArr[0] = {
-    x : x,
-    y : y,
-};
+function createSnake(){ 
 
+    for (var i = 0; i < 2; i++) {
+        var snake = {
+        x : x,
+        y : y, 
+        }
+        x += 20   
+
+        snakeArr.push(snake);
+    }
+}
+createSnake();
+
+function paintSnake(){
+    for(var i = 0; i < snakeArr.length; i++){
+        
+        ctx.fillStyle = "#E0F73A";
+        ctx.fillRect(snakeArr[i].x, snakeArr[i].y, 20, 20);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(snakeArr[i].x, snakeArr[i].y, 20, 20);
+    }
+}
 
 function paintApple() {
 
@@ -39,24 +57,18 @@ function paintApple() {
     ctx.strokeRect(apple.x, apple.y, 20, 20);
 }
 
-function paintSnake(){
+function snakeLogic(){
 
-    for(var i = 0; i < snakeArr.length; i++){
-        
-        ctx.fillStyle = "#E0F73A";
-        ctx.fillRect(snakeArr[i].x, snakeArr[i].y, 20, 20);
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(snakeArr[i].x, snakeArr[i].y, 20, 20);
-    }
-    
     let snakeHead = {
         x : snakeArr[0].x + dx,
         y : snakeArr[0].y + dy,
     }
-     
+
+    if(isPressed){
         snakeArr.pop();
-        snakeArr.unshift(snakeHead);        
-    
+        snakeArr.unshift(snakeHead); 
+    }       
+      
     /* if snake eats apple */
     if(snakeHead.x == apple.x && snakeHead.y == apple.y){
         snakeArr.push(snakeHead);
@@ -64,8 +76,9 @@ function paintSnake(){
 
         apple.x = Math.floor(Math.random()*30)*20;
         apple.y = Math.floor(Math.random()*30)*20;
-    } 
- 
+
+    }
+    paintSnake();
 }
 
 function trackScore(){
@@ -74,20 +87,42 @@ function trackScore(){
     ctx.fillText("Score: " + score, 5, 595);
 }
 
+function gameOver(){
+
+    var snakeH = snakeArr[0];
+    for(var i = 2; i < snakeArr.length; i++){
+        if(snakeH.x + dx === snakeArr[i].x && snakeH.y + dy === snakeArr[i].y)return true; 
+    }
+        if(snakeH.x + dx > w || snakeH.x + dx < 0 && snakeH.y + dy > h || snakeH.y + dy < 0)return true;
+}
+
+/*function resetGame (){
+    var conf = confirm("You died \nNew game?")
+    if(conf === true){
+    var getEl = document.getElementById("snakeG")
+    }
+}*/ 
+
 function draw(){
-    setTimeout(function() {
+
+    var drawOn = setTimeout(function() {
+        
         requestAnimationFrame(draw)
         ctx.clearRect(0, 0, w, h);
-        paintSnake();
         paintApple();
+        snakeLogic();
         trackScore();
-
+        gameOver();
         }, 1000/10)
+        
+        if(gameOver()){   
+        clearTimeout(drawOn);         
+    }  
 } 
         
 /* pressing arrow keys to control the snake */
-document.onkeydown = function usingKey(event) {   
-    
+document.onkeydown = function usingKey(event) {
+    isPressed = true;
 
     /* up key */
     if(event.keyCode === 38 && !(dy === 20)){
